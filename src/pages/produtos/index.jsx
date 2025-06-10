3a48459b5083ebff6ed8a2e7eaaa93bc668bcc14
Mikelemag <mikelepinto2017@gmail.com>
2025-06-10 10:41:08 -0300

    token de validacao

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,9 +1,22 @@
 import PageWrapper from "@/components/PageWrapper";
+import instance from "@/instance/api";
+import { useEffect, useState } from "react";
 
 export default function Produtos(){
+
+    const [produtos, setProdutos] = useState([])
+    useEffect(() => {
+        async function getProdutos(){
+        const response = await instance.get('/produtos')
+        setProdutos(response.data)
+        }
+
+        getProdutos()
+    }, [])
+
     return(
     <PageWrapper>
-        <p>Ola mundo</p>
+        <p>Olá mundo!</p>
     </PageWrapper>
 
     )
